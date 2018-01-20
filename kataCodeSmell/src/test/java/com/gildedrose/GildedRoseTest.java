b89e34649e0f8ae26a4cfe194948564e0adeb52a
hugojuradodsindigo <hugo@dsindigo.mx>
2018-01-19 18:02:52 -0600

    Correccion de prueba fix
    Se decrementan selling y quality en 1 al final del día

--- kataCodeSmell/src/test/java/com/gildedrose/GildedRoseTest.java
+++ kataCodeSmell/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,26 +1,22 @@
 package com.gildedrose;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class GildedRoseTest {
 
-  Item[] items = new Item[]{ new Item("Sulfuras, Hand of Ragnaros", 1, 1) };
-
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("fixme", 0, 0) };
+    public void decrementsItemsQualityAndSellInBy1() {
+        Item[] items = new Item[] { 
+                new Item("fixme", 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        
+        Item item = app.items[0];
+        
+        assertEquals("fixme", item.name);
+        assertEquals(9, item.sellIn);
+        assertEquals(19, item.quality);
     }
-
-    @Test
-    public void decrementarItemSulfuras(){
-      GildedRose app = new GildedRose(this.items);
-      Item item = app.decrementarSellInSulfuras(app.items[0]);
-      assertEquals(0, item.sellIn);
-      assertEquals("Sulfuras, Hand of Ragnaros", item.name);
-    }
+    
 }
