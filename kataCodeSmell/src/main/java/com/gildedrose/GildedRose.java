d44123f918023f1dafb8f16db729250e15ecf8fe
hugojuradodsindigo <hugo@dsindigo.mx>
2018-01-24 11:26:49 -0600

    Rectorizacion incremento Quality fix
    Se refactorizo el incremento de Quality,  embebiendo la validacion de el maximo de Quality (50)

--- kataCodeSmell/src/main/java/com/gildedrose/GildedRose.java
+++ kataCodeSmell/src/main/java/com/gildedrose/GildedRose.java
@@ -1,9 +1,10 @@
 package com.gildedrose;
 
 class GildedRose {
-    
+
     private final int MIN_QUALITY = 0;
-    
+    private final int MAX_QUALITY = 50;
+
     public final Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,68 +13,66 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if ( !isAgedBrie(items[i]) && !isBackstagePasses(items[i]) ) {
+            if (!isAgedBrie(items[i]) && !isBackstagePasses(items[i])) {
                 decreaseQuality(items[i]);
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                increaseQuality(items[i]);
 
-                    if ( isBackstagePasses(items[i]) ) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                if (isBackstagePasses(items[i])) {
+                    if (items[i].sellIn < 11) {
+                        increaseQuality(items[i]);
+                    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                    if (items[i].sellIn < 6) {
+                        increaseQuality(items[i]);
                     }
                 }
             }
 
-            if ( !isSulfuras(items[i]) ) {
+            if (!isSulfuras(items[i])) {
                 decreaseSellIn(items[i]);
             }
 
             if (items[i].sellIn < 0) {
-                if ( !isAgedBrie(items[i]) ) {
-                    if ( !isBackstagePasses(items[i]) ) {
+                if (!isAgedBrie(items[i])) {
+                    if (!isBackstagePasses(items[i])) {
                         decreaseQuality(items[i]);
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    increaseQuality(items[i]);
                 }
             }
         }
     }
-    
+
     public void decreaseQuality(final Item item) {
-        if ( item.quality > MIN_QUALITY ) {
-            if ( !isSulfuras(item) ) {
-                item.quality --;
+        if (item.quality > MIN_QUALITY) {
+            if (!isSulfuras(item)) {
+                item.quality--;
             }
         }
     }
-    
+
     public void decreaseSellIn(final Item item) {
-        item.sellIn --;
+        item.sellIn--;
     }
-    
+
+    private void increaseQuality(Item item) {
+        if (item.quality < MAX_QUALITY) {
+            item.quality++;
+        }
+    }
+
     private boolean isAgedBrie(Item item) {
         return item.name.equals("Aged Brie");
     }
-    
+
     private boolean isBackstagePasses(Item item) {
         return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
-    
+
     private boolean isSulfuras(Item item) {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
