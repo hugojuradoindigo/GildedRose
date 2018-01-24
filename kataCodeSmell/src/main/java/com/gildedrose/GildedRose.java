a716d703b60bed39a3820335ce21f17bce31c104
hugojuradodsindigo <hugo@dsindigo.mx>
2018-01-24 10:50:09 -0600

    Refactorizacion de validacion de productos fix
    Se refactorizo la validacion de los productos Sulfuras, Aged brie y Backstage passes

--- kataCodeSmell/src/main/java/com/gildedrose/GildedRose.java
+++ kataCodeSmell/src/main/java/com/gildedrose/GildedRose.java
@@ -10,10 +10,9 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if ( !isAgedBrie(items[i]) && !isBackstagePasses(items[i]) ) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if ( !isSulfuras(items[i]) ) {
                         decreaseQuality(items[i]);
                     }
                 }
@@ -21,7 +20,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if ( isBackstagePasses(items[i]) ) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -37,15 +36,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if ( !isSulfuras(items[i]) ) {
                 decreaseSellIn(items[i]);
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if ( !isAgedBrie(items[i]) ) {
+                    if ( !isBackstagePasses(items[i]) ) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if ( !isSulfuras(items[i]) ) {
                                 decreaseQuality(items[i]);
                             }
                         }
@@ -68,4 +67,17 @@ class GildedRose {
     public void decreaseSellIn(final Item item) {
         item.sellIn --;
     }
+    
+    private boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
+    }
+    
+    private boolean isBackstagePasses(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+    
+    private boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
 }
