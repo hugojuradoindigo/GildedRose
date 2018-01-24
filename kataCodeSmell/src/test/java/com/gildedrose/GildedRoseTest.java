c0083603b5069578b0223526526df66a9a02a552
hugojuradodsindigo <hugo@dsindigo.mx>
2018-01-24 10:29:38 -0600

    Prueba degradar 2 veces mas rapido fix
    Se implemento una prueba unitaria para la degradacion de quality 2 veces mas una vez que sellIn ha pasado

--- kataCodeSmell/src/test/java/com/gildedrose/GildedRoseTest.java
+++ kataCodeSmell/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -19,4 +19,20 @@ public class GildedRoseTest {
         assertEquals(19, item.quality);
     }
     
+    @Test
+    public void sellByDateHasPassedQualityDegradesTwiceAsFast() {
+        Item[] items = new Item[] { 
+                new Item("fixme", -1, 20) };
+        
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        
+        Item item = app.items[0];
+        
+        assertEquals("fixme", item.name);
+        assertEquals(-2, item.sellIn);
+        assertEquals(18, item.quality);
+    }
+    
+    
 }
